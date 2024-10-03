84ee41f8ab69a864da095cf93a37340e626a4a91
Adelina Gutu <w1705972@gmail.com>
2024-10-03 12:06:19 +0100

    validations

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,9 @@
 class Car < ApplicationRecord
   belongs_to :owner
+  has_many :reviews, dependent: :destroy
+  has_many :favourites
+
+  validates :brand, :model, :fuel, presence: true
+  validates :year, presence: true, numericality: {only_integer: true}
+
 end
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,2 +1,6 @@
 class Owner < ApplicationRecord
+  has_many :cars, dependent: :destroy
+
+  validates :nickname, presence: true, uniqueness: true
+  
 end
